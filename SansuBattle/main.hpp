1899bc2a583e10bc56c74ca548c4bcb84a15ad0a
hj-g18005 <hj-g18005@sist.ac.jp>
2020-05-21 11:19:11 +0900

    プロジェクト ファイルを追加します。

---
+++ SansuBattle/main.hpp
@@ -0,0 +1,25 @@
+//main.hpp
+
+#pragma once
+
+
+//#################### ヘッダファイル読み込み #####################
+#include "DxLib.h"
+
+//########## マクロ定義 ##########
+#define GAME_LEFT	  0		//画面の横の始点
+#define GAME_TOP	  0		//画面の縦の始点
+#define GAME_WIDTH	880		//画面の横の大きさ
+#define GAME_HEIGHT	640		//画面の縦の大きさ
+#define GAME_COLOR	32		//画面のカラービット
+
+#define GAME_WINDOW_NAME	"さんすうバトル"	//ウィンドウのタイトル
+#define GAME_WINDOW_MODECHANGE	TRUE			//TRUE：ウィンドウモード / FALSE：フルスクリーン
+
+#define SET_WINDOW_ST_MODE_DEFAULT			0	//デフォルト
+#define SET_WINDOW_ST_MODE_TITLE_NONE		1	//タイトルバーなし
+#define SET_WINDOW_ST_MODE_TITLE_FLAME_NONE	2	//タイトルバーとフレームなし
+#define SET_WINDOW_ST_MODE_FLAME_NONE		3	//フレームなし
+
+//############## プロトタイプ宣言 #############
+void Delete_Class();		//クラスを削除する処理
