6e357c27aad1dd693f54bce1ab2f502a9f405d9a
hj-g18005 <hj-g18005@sist.ac.jp>
2020-06-22 13:23:31 +0900

    問題作成処理修正。

--- SansuBattle/Q_ADD.cpp
+++ SansuBattle/Q_ADD.cpp
@@ -37,14 +37,14 @@ void Q_Add::CreateQuestion(int gamelevel)
 	case (int)STAGE_LEVEL_NORMAL:	//普通の時
 
 		q_min = 10;					//最小値1
-		q_max = 20;	//最大値9
+		q_max = 20;	//最大値20
 
 		break;	//普通の時
 
 	case (int)STAGE_LEVEL_HARD:	//難しいの時
 
 		q_min = 20;					//最小値1
-		q_max = 30;	//最大値9
+		q_max = 30;	//最大値30
 
 		break;	//難しいの時
 
@@ -55,13 +55,8 @@ void Q_Add::CreateQuestion(int gamelevel)
 
 	int num1 = 0, num2 = 0;		//問題を入れる変数
 
-	num1 = GetRand(q_max);			//問題を生成
-	num2 = GetRand(q_max);			//問題を生成
-
-	if (num1 < q_min)	//最小値より小さければ
-		num1 += q_min;	//最小値より大きくなるよう設定
-	if (num2 < q_min)	//最小値より小さければ
-		num2 += q_min;	//最小値より大きくなるよう設定
+	num1 = GetRand(q_max - q_min) + q_min;			//問題を生成
+	num2 = GetRand(q_max - q_min) + q_min;			//問題を生成
 
 	this->Anser = num1 + num2;	//問題の計算結果を答えに格納
 	this->Q_Text = (std::to_string(num1) + "＋" + (std::to_string(num2) + "＝？"));		//問題文を設定
